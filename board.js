ccc29087aabaad7f0801fcbc7813f864fbd4a188
adhpawal <pawaladhikari@lftechnology.com>
2015-07-04 00:57:03 +0545

    #3 SwimLane JS merged

--- board.js
+++ board.js
@@ -66,4 +66,30 @@ $(document).ready(function(){
 			}
 		});
 	}
+
+	//Defines Draggable Element
+	$(".draggable").draggable({
+		revert: "invalid",
+		zIndex: 10000,
+		appendTo: "body",
+		stack: ".draggable"
+	});
+
+	//Defines Droppable Element
+	$(".droppable").droppable({
+		tolerance: "intersect",
+		drop: function (event, ui) {
+		}
+	});
+
+	$("#github-user-setting").on("submit", function(event){
+		$(".alert").hide();
+		event.preventDefault();
+		if(!$("#repoId").val() || !$("#milestone").val() || !$("#lane1").val() || !$("#lane2").val() || !$("#lane3").val() || !$("#lane4").val()){
+			$(".alert-danger").show();
+			return false;
+		}
+		$(".alert-success").show();
+		//reload page
+	});
 });
