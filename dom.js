654f7220a20f6a484566782568a9b3b55430ae05
coolnimesh43 <nimesh_mishra@lftechnology.com>
2015-07-03 18:29:36 +0545

    #1: Add basic structure for extension. Added filter for pull request

---
+++ dom.js
@@ -0,0 +1,38 @@
+
+$(document).ready(function(){
+	console.log("dom called");
+
+	chrome.storage.sync.get("filterString",function(data){
+		console.log("data from storage: "+data.toString());
+		if(data){
+			filterString=data.filterString;
+			console.log(filterString);
+			hideFilterPages(filterString);
+		}
+		else{
+			console.log("no data for filter");
+		}
+	});
+
+	/**
+	* Method to hide pages described in filter list.
+	* @param filterString The string containing list of filter pages separated by comma.	
+	**/
+	function hideFilterPages(filterString){
+		console.log("hiding filter pages. : "+filterString);
+		if(filterString){
+			var filterPages=filterString.split(",");
+			for(var i=0; i<filterPages.length;i++){
+				$('div[data-path*="'+filterPages[i]+'"]').each(function (){
+					$(this).parent().hide();
+				});
+			}
+		}
+	}    
+
+
+	//use this method to send message from content page
+	// chrome.runtime.sendMessage({action:"getDOM"},function(response){
+				// console.log(response);
+	// });
+});
